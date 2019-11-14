fe8a3e897f694a7b23e34d895618cb00694157ab
lucho1503 <lmmc.1503@gmail.com>
2019-11-14 02:56:49 +0000

    __init__.py_file

--- models/base_model.py
+++ models/base_model.py
@@ -1,42 +1,45 @@
 #!/usr/bin/python3
+"""
+class base - this class defines all common attributes/methods for other classes
+"""
 
-import models
-import json
 import uuid
+from models import datetime
 from datetime import datetime
 
 class BaseModel:
-
+    """ Class Base Model """
     def __init__(self, *args, **kwargs):
+        """ create a instance with an representation of dictionary """
         if kwargs:
+            date_format = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
-                if key is '__class__':
-                    continue
-                if key is 'created_at' or key is 'updated_at':
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(kwargs[key], date_format)
+                if key != '__class__':
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+            self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
+        """ print [class name] (id) (__dict__) """
         idd = self.id
         diction = self.__dict__
         name = self.__class__.__name__
         return "[{}] ({}) {}".format(name, idd, diction)
 
     def save(self):
+        """ update with the current datetime """ 
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
-        my_dict = {}
-        for key, value in self.__dict__.items():
-            if key in ['created_at', 'updated_at']:
-                my_dict[key] = value.isoformat()
-            else:
-                my_dict[key] = value
+        """ returns a dictionary of a instance """
+        my_dict = self.__dict__.copy()
         my_dict['__class__'] = self.__class__.__name__
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
         return my_dict
