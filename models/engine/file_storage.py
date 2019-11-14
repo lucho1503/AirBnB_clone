9374e03ca673d89c68668d2adc0a86e9800fbcdf
lucho1503 <lmmc.1503@gmail.com>
2019-11-14 21:03:39 +0000

    add dicc

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,8 +6,14 @@ import json
 import os
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
-dictionary = {'BaseModel': BaseModel, 'User': User}
+dictionary = {'BaseModel': BaseModel, 'User': User, 'State': State, 'City': City,
+              'Amenity': Amenity, 'Place': Place, 'Review': Review}
 
 
 class FileStorage:
